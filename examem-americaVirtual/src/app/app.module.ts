de77a0c796ecec766333bda9e8091c4991ee0f75
Tomy97 <64372041+Tomy97@users.noreply.github.com>
2021-04-10 20:49:18 -0300

    mas definiciones del front y terminado. Solo quedaron detalles

--- examem-americaVirtual/src/app/app.module.ts
+++ examem-americaVirtual/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ContainerComponent } from './components/container/container.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ServicioComponent } from './components/servicio/servicio.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { LoginComponent } from './components/login/login.component';
 import { DiasComponent } from './components/dias/dias.component';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [AppComponent, ContainerComponent, HeaderComponent, ServicioComponent, FooterComponent, LoginComponent, DiasComponent],
-  imports: [BrowserModule, NgbModule],
+  declarations: [AppComponent, ContainerComponent, HeaderComponent, ServicioComponent, FooterComponent, DiasComponent],
+  imports: [BrowserModule, NgbDropdownModule],
   providers: [],
   bootstrap: [AppComponent],
 })
